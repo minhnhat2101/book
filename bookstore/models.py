df6f03532976103613cc8c93a2cd52f274fc13db
tinhph2 <tinhph2@fe.edu.vn>
2023-07-17 19:04:52 +0700

    adcode

--- bookstore/models.py
+++ bookstore/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+class TacGia(models.Model):
+    id = models.AutoField(primary_key=True)
+    tenTacGia = models.CharField(max_length=250, db_index=True)
+    
 # Create your models here.
+class Sach(models.Model):
+    id = models.AutoField(primary_key=True)
+    tenSach = models.CharField(max_length=250, db_index=True)
+    gia = models.IntegerField()
+    tacGia = models.ForeignKey(TacGia, related_name='book', on_delete=models.CASCADE, null=True)
+
+
