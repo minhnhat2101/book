73fcb15a2711013b7d0c1f09635d21eb3cc415ce
tinhph2 <tinhph2@fe.edu.vn>
2023-07-17 19:56:48 +0700

    adcode

--- bookstore/views.py
+++ bookstore/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
+from .models import Sach
 # Create your views here.
 
 def danh_sach(request):
-    return render(request, "bookstore/index.html")
+    sach = Sach.objects.all()
+    context = {
+        'sach':sach
+    }
+    return render(request, "bookstore/index.html",context)
