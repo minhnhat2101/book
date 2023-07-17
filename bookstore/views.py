be68f5bf45a0ce5aa8866864152f675bfd78cbab
tinhph2 <tinhph2@fe.edu.vn>
2023-07-17 20:20:36 +0700

    adcode

--- bookstore/views.py
+++ bookstore/views.py
@@ -8,3 +8,19 @@ def danh_sach(request):
         'sach':sach
     }
     return render(request, "bookstore/index.html",context)
+
+
+
+def timsach(request):        
+    if request.method == 'GET': # this will be GET now      
+        ten_sach =  request.GET.get('ten') # do some research what it does       
+        status = Sach.objects.filter(tenSach__icontains=ten_sach) # filter returns a list so you might consider skip except part
+        context = {
+        'sach':status
+         }
+        return render(request, "bookstore/index.html",status)
+    else:
+        context = {
+        'sach':""
+         }
+        return render(request,"search.html",{})
